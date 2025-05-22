b41150269d1e73fc32c58900f773eb6ad2ad5c54
RahimCHELLAL <AREZ3BLACKOPS@gmail.com>
2025-05-22 11:26:00 +0100

    First integration of the chatbot

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print(hello world)
+import streamlit as st
+
+st.set_page_config(page_title="Business Health Chatbot")
+
+st.chat_message("test")
+user_input = st.text_input("Let me help you, give me the core of yours problems in your buisness, and I will help you get a diagnosis and solutions")
+
+if user_input:
+    message = st.chat_message("human")
+    message.write(f"You asked: {user_input}")
+
+    answer = st.chat_message("ai")
+    answer.write("Answer will appear here...")
