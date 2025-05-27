736fb5074db12a91174f213365b6cd21a5295c77
RahimCHELLAL <AREZ3BLACKOPS@gmail.com>
2025-05-27 20:53:43 +0100

    Ajoutit Hello for testing

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import random
 import google.generativeai as genai
 from google.api_core.exceptions import ResourceExhausted
 
-
+#Hello !
 def read_text_from_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
