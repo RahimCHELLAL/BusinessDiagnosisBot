7c4581dc007c3b4afedbd545938d740a5a6c762a
RahimCHELLAL <AREZ3BLACKOPS@gmail.com>
2025-05-30 16:53:40 +0100

    implementation of different function to extract text from pdf

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def read_text_from_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
 
-def chunk_text(text, chunk_size=1000, overlap=200):
+def chunk_text(text, chunk_size=1000, overlap=200): #TODO: better chunking, by extending the limit untill the phrase
     chunk = []
     start = 0
 
